9e9e83891700ec8444a356afef47056e40b7f638
TiThuthur <77291107+TiThuthur@users.noreply.github.com>
2022-05-13 09:27:18 +0200

    ajout de la classe imbriqué Genre

--- merchex/listing/models.py
+++ merchex/listing/models.py
@@ -2,16 +2,23 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 # Create your models here.
 
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
-    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900),MaxValueValidator(2021)])
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
-    official_homepage = models.fields.URLField(null=True,blank=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 
 class Notification(models.Model):
